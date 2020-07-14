9558fb45e7ee48bb6fa0b7011206a723bc931816
Ross Blumhorst <>
2020-07-13 21:58:29 -0500

    forgot brackets whoops

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -13,7 +13,7 @@ class QuizQuestion extends Component {
       </section>
       <section>
         <ul>
-          <li>this.props.quiz_question.answer_options[0]</li>
+          <li>{this.props.quiz_question.answer_options[0]}</li>
         </ul>
       </section>
     </main>)
